004b1785541fd0ea0a1a6ac8c54c3a1ca6045c01
alex-braun <braunacb@gmail.com>
2017-01-16 22:13:31 -0500

    Added geolocation with ipinfo.
    User location automatically.

--- app/initializers/geo-location.js
+++ app/initializers/geo-location.js
@@ -1,9 +1,10 @@
 export function initialize(application) {
   // application.inject('route', 'foo', 'service:foo');
-   application.inject('route', 'geo', 'service:geo-location');
+   application.inject('route', 'geo-location', 'service:geo-location');
+  // console.log('I go first');
 }
 
 export default {
-  name: 'geo-location',
+  name: 'geoLocation',
   initialize
 };
--- app/services/geo-location.js
+++ app/services/geo-location.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 // import ENV from 'show-hock-app/config/environment';
 
 export default Ember.Service.extend({
+events: Ember.inject.service('region-events-onload'),
+
 ///UNCOMMENT THIS WHEN YOU ARE READY!!!
   city: null,
   region: null,
@@ -13,25 +15,12 @@ export default Ember.Service.extend({
       method: 'GET',
       url: "http://ipinfo.io/json"
     })
-    .then(response => {
+    .done(response => {
 
       this.set('city', response.city);
       this.set('region', response.region);
       this.set('country', response.country);
-    })
-    .catch(error => {
-      console.error(error);
     });
   }
-});
 
-      // .then(() =>
-      //   $.ajax({
-      //     method: 'GET',
-      //     url: ENV.apiHost + '/geolocation/' +
-      //     userCity+userRegion+userCountry
-      //   })
-      //   .then(response => {
-      //     console.log(response);
-      //   })
-      // );
+});
