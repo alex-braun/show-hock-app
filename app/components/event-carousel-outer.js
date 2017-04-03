1d091ad9930bf49c2a4404782c7a1492ab66f412
alex-braun <braunacb@gmail.com>
2017-04-02 20:08:33 -0400

    Added index route with carousel, styling
    Basic home page design created.

---
+++ app/components/event-carousel-outer.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['event-carousel-outer'],
+
+  slideFrom: 0,
+  slideTo: 0,
+  slideClick: false,
+
+  uniqueArtists: Ember.computed.uniqBy('events', 'headlineArtist'),
+  sortedEvents: Ember.computed.sort('uniqueArtists', 'sortAttrs'),
+  sortAttrs: ['popularity:desc'],
+
+  updateSlideNumber: (function() {
+    let slideFrom = this.get('slideFrom');
+    let slideTo = this.get('slideTo');
+    let slideClick = this.get('slideClick');
+    this.set('slideFrom', slideFrom);
+    this.set('slideTo', slideTo);
+
+    if (slideFrom === 4 && slideTo === 0) {
+      if (slideClick === false) {
+        Ember.$(".carousel").carousel('next');
+      } else {
+        Ember.$(".carousel").carousel(slideTo);
+      }
+    }
+    else if (slideFrom === 0 && slideTo === 4) {
+      if (slideClick === false) {
+        Ember.$(".carousel").carousel('prev');
+    } else {
+        Ember.$(".carousel").carousel(slideTo);
+      }
+    } else {
+      Ember.$(".carousel").carousel(slideTo);
+    }
+  }).observes('slideTo'),
+
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      this.$(".carousel").carousel({
+        interval: false
+      });
+    });
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this.$('.carousel').off('.carousel');
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,68 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Route.extend({
+
+  // regionId: null,
+  randArray: null,
+  geoLocation: Ember.inject.service(),
+
+  beforeModel() {
+    this._super(...arguments);
+    let clientIp = this.get('geoLocation').clientIp;
+      if (clientIp === null || clientIp === undefined) {
+        return this.get('geoLocation').getIp()
+        .then(() =>
+        this.get('geoLocation').getRegion(this.get('geoLocation').clientIp));
+      } else {
+        return this.get('geoLocation').getRegion(clientIp);
+      }
+  },
+
+  model() {
+    let regionId = this.get('geoLocation').regionId;
+    return this.get('store').findRecord('region', regionId, { adapterOptions: { page: 1 }
+    })
+    .then((result) => {
+      // let mostPop = result.data.event.sortBy('popularity').reverse();
+      // let mostPop = result.sortBy('popularity').reverse();
+      // console.log(mostPop);
+      let meta = result.get('meta');
+      return meta, result;
+    });
+  },
+
+
+
+//   similar: this.get('store').findRecord('similar-artist', param.artist_id)
+//   .then((result) => {
+//     let arr = [];
+//     for (let i = 0; i < 10; i++) {
+//       arr.push([i]);
+//     }
+//        let selection = result.data.artist.objectsAt(arr);
+//        selection = selection.filter((element) => {
+//          return element !== undefined;
+//        });
+//        return selection;
+//   }),
+// });
+  setupController(controller, model) {
+    controller.set('model', model);
+  },
+  // afterModel(model) {
+  //   let len = model.data.event.length;
+  //   let randArray = [];
+  //   for (let i = 0; i < 4; i++) {
+  //     randArray.push(Math.floor(Math.random()*(len-0)));
+  //   }
+  //   return this.set('randArray', randArray);
+  // }
+  actions: {
+    getRegionShows(val) {
+      console.log(val);
+      this.transitionTo('region.event.results', val);
+    }
+  }
 });
