9fcf8f314e18f7bc1a17bca14d04ede0e3afd92a
alex-braun <braunacb@gmail.com>
2017-04-22 22:36:11 -0400

    Add changes to environment for production
    Preparing for deployment.

--- config/environment.js
+++ config/environment.js
@@ -6,8 +6,8 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
-    apiHost: 'https://obscure-inlet-58315.herokuapp.com',
-    // apiHost: 'http://localhost:4741',
+    // apiHost: 'https://obscure-inlet-58315.herokuapp.com',
+    apiHost: 'http://localhost:4741',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -66,7 +66,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = 'show-hock-app';
+    ENV.locationType = 'hash';
+    ENV.apiHost = 'https://obscure-inlet-58315.herokuapp.com';
   }
 
   return ENV;
